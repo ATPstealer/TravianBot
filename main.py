d2ec59ea8b4354ddcde4c18d7a003501fac4b994
atpstealer <forpubmail@gmail.com>
2023-08-19 11:48:33 +0300

    buildings

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ from auth import Request
 from village import Village
 from bs4 import BeautifulSoup
 
+get = Request()
 
-def get_villages():
-    get = Request()
+
+def get_villages(get):
     soup = BeautifulSoup(get.request("/dorf1.php").text, "html.parser")
     divs = soup.find_all(class_="listEntry")
     return [div['data-did'] for div in divs]
 
 
-village_ids = get_villages()
-print(village_ids)
+village_ids = get_villages(get)
+for village_id in village_ids:
+    village = Village(village_id, get)
 
 
 
-# village = Village("17901")
 
--- village.py
+++ village.py
@@ -1,17 +1,66 @@
-from auth import Request
+from bs4 import BeautifulSoup
+
+
+class Resource:
+    def __init__(self, land_type, level, slot, gid):
+        self.land_type = land_type
+        self.level = level
+        self.slot = slot
+        self.gid = gid
+
+
+class Building:
+    def __init__(self, building_type, level, slot, gid):
+        self.building_type = building_type
+        self.level = level
+        self.slot = slot
+        self.gid = gid
 
 
 class Village:
-    def __init__(self, village_id):
+    def __init__(self, village_id, get):
+        print(village_id)
         self.village_id = village_id
-        self.get = Request()
+        self.get = get
+        self.resources = []
+        self.buildings = []
         self.get_resources()
+        self.show_resources()
         self.get_buildings()
+        self.show_buildings()
 
     def get_resources(self):
-        resources_page = self.get.request("/dorf1.php?newdid=" + self.village_id)
-        print(resources_page.text)
+        soup = BeautifulSoup(self.get.request("/dorf1.php?newdid=" + self.village_id).text, "html.parser")
+        resource_fields = soup.find(id="resourceFieldContainer")
+        for resource_field in resource_fields.find_all(class_="level"):
+            classes = resource_field['class']
+            if "gid1" in classes:
+                land_type = "Woodcutter"
+            elif "gid2" in classes:
+                land_type = "Clay Pit"
+            elif "gid3" in classes:
+                land_type = "Iron Mine"
+            elif "gid4" in classes:
+                land_type = "Cropland"
+            level_str = next(c for c in classes if c.startswith('level') and c != 'level')
+            level = int(''.join(filter(str.isdigit, level_str)))
+            slot_str = next(c for c in classes if c.startswith('buildingSlot'))
+            slot = int(''.join(filter(str.isdigit, slot_str)))
+            gid_str = next(c for c in classes if c.startswith('gid'))
+            gid = int(''.join(filter(str.isdigit, gid_str)))
+            self.resources.append(Resource(land_type, level, slot, gid))
 
     def get_buildings(self):
-        resources_page = self.get.request("/dorf2.php?newdid=" + self.village_id)
-        print(resources_page.text)
+        soup = BeautifulSoup(self.get.request("/dorf2.php?newdid=" + self.village_id).text, "html.parser")
+        buildings_divs = soup.find(id="villageContent")
+        for div in buildings_divs.find_all(class_="buildingSlot"):
+            level = div.find("a")['data-level'] if div['data-gid'] != "0" else 0
+            self.buildings.append(Building(div['data-name'], int(level), int(div['data-aid']), int(div['data-gid'])))
+
+    def show_resources(self):
+        for res in self.resources:
+            print(res.land_type, res.level, res.slot, res.gid)
+
+    def show_buildings(self):
+        for building in self.buildings:
+            print(building.building_type, building.level, building.slot, building.gid)
